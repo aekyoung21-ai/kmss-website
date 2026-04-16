05b7b9c5909f398d23dd9e59bb7d012f0364beb8
aekyoung21-ai <aekyoung21@gmail.com>
2026-04-16 11:24:22 +0900

    fix: 임원진 소속 전공·학과 괄호 표기 추가 (8명)

--- app/about/executives/page.tsx
+++ app/about/executives/page.tsx
@@ -6,22 +6,22 @@ export const metadata: Metadata = {
 };
 
 const executives = [
-  { role: "회장", name: "선애경", nickname: "두잇맘", org: "고려대학교 공학교육혁신센터", position: "연구교수" },
-  { role: "부회장", name: "이원경", nickname: "Wony", org: "Ludwig-Maximilians-Universität 심리학과", position: "박사과정" },
+  { role: "회장", name: "선애경", nickname: "두잇맘", org: "고려대학교 공학교육혁신센터 (교육행정학 및 고등교육학)", position: "연구교수" },
+  { role: "부회장", name: "이원경", nickname: "Wony", org: "Ludwig-Maximilians-Universität (심리학과)", position: "박사과정" },
   { role: "감사", name: "Rey Jeong", nickname: "레이J", org: "캘리포니아 주립대학교 새크라멘토 (미술학부)", position: "조교수" },
   { role: "감사", name: "진선미", nickname: "가인맘", org: "Indiana University Bloomington (고등교육전공)", position: "박사수료" },
-  { role: "서기", name: "박주영", nickname: "제이씬", org: "이화여자대학교 임상바이오헬스대학원 임상영양학전공", position: "석사과정" },
+  { role: "서기", name: "박주영", nickname: "제이씬", org: "이화여자대학교 임상바이오헬스대학원 (임상영양학전공)", position: "석사과정" },
   { role: "서기", name: "송수연", nickname: "리즈", org: "경기 신천고등학교 (역사교육전공)", position: "박사수료" },
   { role: "이사", name: "김소이", nickname: "세츄츄", org: "충남대학교 미래인재성장연구소", position: "박사수료" },
-  { role: "이사", name: "김예슬", nickname: "푸리맘", org: "서울과학기술대학교 안전공학과", position: "석사과정" },
+  { role: "이사", name: "김예슬", nickname: "푸리맘", org: "서울과학기술대학교 (안전공학과)", position: "석사과정" },
   { role: "이사", name: "김혜린", nickname: "해달맘", org: "서울특별시청 (고려대학교 보건대학원)", position: "석사졸업" },
   { role: "이사", name: "신혜성", nickname: "세요맘", org: "청운대학교 (교육공학전공)", position: "교수" },
-  { role: "이사", name: "이승주", nickname: "노니", org: "서울대학교 보건정책관리학", position: "박사수료" },
-  { role: "이사", name: "이정은", nickname: "온맘", org: "이화여자대학교 평생교육전공", position: "박사과정" },
-  { role: "이사", name: "이한슬기", nickname: "키키", org: "서울대학교 간호대학", position: "박사수료" },
+  { role: "이사", name: "이승주", nickname: "노니", org: "서울대학교 (보건정책관리학)", position: "박사수료" },
+  { role: "이사", name: "이정은", nickname: "온맘", org: "이화여자대학교 (평생교육전공)", position: "박사과정" },
+  { role: "이사", name: "이한슬기", nickname: "키키", org: "서울대학교 (간호대학)", position: "박사수료" },
   { role: "이사", name: "임나래", nickname: "모리", org: "큐레이터 (미학, 예술학)", position: "석사졸업" },
   { role: "이사", name: "임재은", nickname: "라비에", org: "세종여자고등학교 체육교사 (체육교육)", position: "박사수료" },
-  { role: "이사", name: "정지은", nickname: "소라블리", org: "강남대학교 교육학과", position: "박사졸업" },
+  { role: "이사", name: "정지은", nickname: "소라블리", org: "강남대학교 (교육학과)", position: "박사졸업" },
 ];
 
 const roleBadgeColor: Record<string, string> = {
